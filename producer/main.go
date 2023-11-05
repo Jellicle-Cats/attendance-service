60cd70fa7de2169b860a65fa9922df37a1017064
Ryn Parin <ryn.parin@gmail.com>
2023-11-05 18:25:10 +0700

    feat: enable cors

--- producer/main.go
+++ producer/main.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/IBM/sarama"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/spf13/viper"
 )
 
@@ -34,6 +35,13 @@ func main() {
 	accountController := controllers.NewAttendanceController(accountService)
 
 	app := fiber.New()
+
+	app.Use(cors.New(cors.Config{
+        AllowHeaders:     "Origin,Content-Type,Accept,Content-Length,Accept-Language,Accept-Encoding,Connection,Access-Control-Allow-Origin",
+        AllowOrigins:     "*",
+        AllowCredentials: true,
+        AllowMethods:     "GET,POST,HEAD,PUT,DELETE,PATCH,OPTIONS",
+    }))
 
 	app.Post("/checkin", accountController.CheckIn)
 	app.Post("/checkout", accountController.CheckOut)
